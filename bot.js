6a7e2dbecff09b34070e8a45ab6e21a5570fef8c
Andrew George <DevAndrewGeorge@gmail.com>
2018-09-02 02:33:26 +0000

    Move commands to their own modules

--- bot.js
+++ bot.js
@@ -8,6 +8,11 @@ const dialog = require("./dialog");
 const nunjucks = require("nunjucks");
 
 
+// command modules
+const start = require("./commands/start");
+const createcalendar = require("./commands/createcalendar");
+
+
 // writing PID to file
 const pid = require("npid").create("/var/run/hostess.pid", true);
 pid.removeOnExit();
@@ -42,74 +47,51 @@ function setup() {
 }
 
 
-function parseCommand(text) {
+function parseMessageText(text) {
     const args = text.split(" ");
-    return {
-        command: args[0][0] === "/" ? args[0] : "",
-        arguments: args.length > 1 ? args.slice(1) : []
-    }
-}
-
-
-function start(bot, user, chat_id, arguments) {
-    function greet() {
-    }
-
-    function insert_react(err, res, fields) {
-        console.log(err);
-        // TODO: error handling
-        const str = nunjucks.renderString(
-            dialog['responses']['start/0'],
-            { username: user.username }
-        ) + "\n\n" + dialog['tooltips']['start/0'];
-        bot.sendMessage(chat_id, str, {parse_mode: 'Markdown'});
-    }
-
-    function lookup_react(err, res, fields) {
-        // TODO: error handling
-        if (res.length === 0) {
-            database.set_active_calendar(user.id, null, insert_react);
-            return;
-        }
-
-        if (res[0]['calender_title']) {
-            const str = nunjucks.renderString(
-                dialog['responses']['start/2'],
-                { username: user.username, calendar_title: res[0]['calendar_title'] }
-            );
-            bot.sendMessage(chat_id, str, {parse_mode: 'Markdown'});
-        } else {
-            const str = nunjucks.renderString(
-                dialog['responses']['start/1'],
-                { username: user.username }
-            ) + "\n\n" + dialog['tooltips']['start/1'];
-            bot.sendMessage(chat_id, str, {parse_mode: 'Markdown'});
-        }
-    }
-
-    database.get_active_calendar(user.id, lookup_react);
+    parsed = {}
+    parsed["command"] = args[0][0] === "/" ? args[0] : "";
+    parsed["arguments"] = parsed["command"] ? args.slice(1) : args;
+    return parsed;
 }
 
 
 function main() {
+    // bot.on handler
+    function handleMessage(message) {
+        // unified exit callback for all messages received
+        function sendMessage(err, responseText) {
+            bot.sendMessage(chat_id, responseText, {parse_mode: 'Markdown'});
+        }
+
+        const parsed_text = parseMessageText(message.text);
+        const user = message.from,
+            chat_id = message.chat.id,
+            args = parsed_text["arguments"];
+    
+        switch (parsed_text["command"].toLowerCase()) {
+            case "/start":
+                start(user, chat_id, args, database, sendMessage);
+                break;
+            case "/createcalendar":
+                createcalendar(user, chat_id, args, database, sendMessage);
+                break;
+        }
+    
+        logger.debug({
+            user_id: message.from.id,
+            username: message.from.username,
+            timestamp: message.date,
+            chat_id: message.chat.id,
+            action: message.text
+        });
+    }
+
+
     // setting up bot
     const bot = setup();
 
-    bot.on("message", msg => {
-        const command = parseCommand(msg.text);
-        switch (command["command"]) {
-            case "/start":
-                start(bot, msg.from, msg.chat.id, command["arguments"]);
-        }
-
-        logger.debug({
-            user_id: msg.from.id,
-            username: msg.from.username,
-            timestamp: msg.date,
-            chat_id: msg.chat.id,
-            action: msg.text
-        });
-    });
+    bot.on("message", handleMessage);
 }
 
 main();
--- dialog.js
+++ dialog.js
@@ -1,9 +1,9 @@
+// requirements
 const fs = require("fs");
-
-const responses = {}
-const tooltips = {}
+const nunjucks = require("nunjucks");
 
 
+// helper functions
 function walk(path) {
     /* returns full paths of files recursively nested under path */
     const stat = fs.statSync(path);
@@ -20,16 +20,38 @@ function walk(path) {
     return children;
 }
 
-const response_path = __dirname + "/dialog/responses";
-const tooltip_path  = __dirname + "/dialog/tooltips";
-module.exports = {
-    responses: walk(response_path).reduce(function(prev, curr) {
-        prev[curr.replace(response_path + "/", "")] = fs.readFileSync(curr, "utf8");
-        return prev;
-    }, {}),
 
-    tooltips: walk(tooltip_path).reduce(function(prev, curr) {
-        prev[curr.replace(tooltip_path + "/", "")] = fs.readFileSync(curr, "utf8");
-        return prev;
-    }, {})
+function render(response_key, tooltip_keys, context) {
+    let template = responses[response_key] + "\n\n";
+    if (typeof tooltip_keys === "string") {
+        template += tooltips[tooltip_keys];
+    } else if (tooltip_keys) {
+        template += tooltip_keys.reduce(function(str, key) {
+            return str + tooltips[key] + "\n";
+        }, "").replace(/\n$/, "");
+    }
+
+    return nunjucks.renderString(template, context);
+}
+
+
+// variable declarations
+const response_path = __dirname + "/dialog/responses";
+const responses =  walk(response_path).reduce(function(prev, curr) {
+    prev[curr.replace(response_path + "/", "")] = fs.readFileSync(curr, "utf8");
+    return prev;
+}, {});
+
+const tooltip_path  = __dirname + "/dialog/tooltips";
+const tooltips = walk(tooltip_path).reduce(function(prev, curr) {
+    prev[curr.replace(tooltip_path + "/", "")] = fs.readFileSync(curr, "utf8");
+    return prev;
+}, {});
+
+
+
+module.exports = {
+    responses: responses,
+    tooltips: tooltips,
+    render: render
 }
